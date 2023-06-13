4528d4241a6a0341586e1eb4c414ab1ea6d040e5
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-14 01:09:20 +0400

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile, stat } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, './files/fresh.txt');
+    
+    try {
+        await writeFile(pathToFile, 'I am fresh and young', { flag: 'wx' });
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
