28dc8016957cbbf2dc7689d2aec4c7afa2172877
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-14 23:24:06 +0400

    feat: cli/args.js functionality

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const argsFormatted = args
+        .map((arg, index, arr) => {
+            let result = '';
+
+            if (arg.includes('--')) {
+                arg.replace('--', '');
+                result = `${arg.replace('--', '')} is ${arr[index + 1]}`;
+            }
+
+            return result;
+        })
+        .filter(Boolean);
+
+    console.log(argsFormatted.join(', '));
 };
 
 parseArgs();
