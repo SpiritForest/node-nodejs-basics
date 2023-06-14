718081370ba534899037abc9a3e87ca175b61a11
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-14 22:46:24 +0400

    feat: list.js functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { readdir } from 'fs/promises';
+import * as url from 'url';
+import path from 'path';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const pathToDir = path.join(__dirname, './files');
+        const files = await readdir(pathToDir);
+
+        console.log(files);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
