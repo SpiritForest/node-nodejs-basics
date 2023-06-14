fa8dfadfb30c5bc65948656271c61dd779253588
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-14 22:52:57 +0400

    feat: read.js functionality

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const pathToFile = path.join(__dirname, './files/fileToRead.txt');
+        const fileContent = await readFile(pathToFile, { encoding: 'utf8', flag: 'r' });
+
+        console.log(fileContent);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
