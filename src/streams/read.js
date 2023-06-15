e55390012848dca4e4e35ec66b020c25bedd0c80
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-15 23:51:34 +0400

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'fs';
+import * as url from 'url';
+import path from 'path';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, './files/fileToRead.txt');
+
+    createReadStream(pathToFile).pipe(process.stdout);
 };
 
 await read();
