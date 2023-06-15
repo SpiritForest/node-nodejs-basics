e0e14f15dd5830e5e028b725c942efcadf626901
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-16 00:35:20 +0400

    feat: implement tranform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform, pipeline } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, String(chunk).split('').reverse().join(''));
+        }
+    });
+    pipeline([
+        process.stdin,
+        transformStream,
+        process.stdout
+    ], (err) => {
+        if (err) {
+            process.exit(1);
+        }
+    });
 };
 
 await transform();
