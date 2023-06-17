cb56c00a54d22936011c754485635cb910e8d25a
Dzmitry Zakhar <dzakhar@adnoc.ae>
2023-06-18 01:08:28 +0400

    feat(worker_threads): implement worker.js and main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,51 @@
+import { Worker } from 'worker_threads';
+import path from 'path';
+import * as url from 'url';
+import os from 'os';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const createWorker = (indexOfFibonacciToCalculate) => {
+    const pathToWorkerThread = path.join(__dirname, './worker.js');
+
+    return new Worker(pathToWorkerThread, {
+        workerData: indexOfFibonacciToCalculate
+    });
+}
+
+const listenToWorkerMessage = (worker) => {
+    return new Promise((res) => {
+        worker.on('message', (message) => {
+            worker.terminate();
+            res({
+                status: 'resolved',
+                data: +message,
+            });
+        });
+        worker.on('error', () => {
+            res({
+                status: 'error',
+                data: null,
+            });
+        });
+    });
+};
+
 const performCalculations = async () => {
-    // Write your code here
+    const numberOfCpus = os.cpus().length;
+    const workersResultPromise = [];
+    let indexOfFibonacciToCalculate = 10;
+
+    for (let i = 0; i < numberOfCpus; i += 1) {
+        const worker = createWorker(indexOfFibonacciToCalculate);
+        workersResultPromise.push(listenToWorkerMessage(worker));
+
+        indexOfFibonacciToCalculate += 1;
+    }
+
+    Promise.all(workersResultPromise).then((workerResults) => {
+        console.log(workerResults);
+    });
 };
 
 await performCalculations();
